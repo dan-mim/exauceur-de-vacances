c4cc7759c405110281a374bae0971cf85e3f3663
dan-mim <108183734+dan-mim@users.noreply.github.com>
2022-06-30 19:54:56 +0200

    Update app_kayak_web_v1.py

--- app_kayak_web_v1.py
+++ app_kayak_web_v1.py
@@ -303,7 +303,7 @@ def find_conv_doll_euros():
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     #chrome_options.add_argument("--headless")
     #chrome_options.add_argument("--disable-dev-shm-usage")
-    #chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--no-sandbox")
     browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     ###    ###
     #browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())#, options=option_browser)
