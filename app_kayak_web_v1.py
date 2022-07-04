8a020374df2756c318bae7e95bc71e44c3e06417
dan-mim <108183734+dan-mim@users.noreply.github.com>
2022-07-04 18:00:14 +0200

    Update app_kayak_web_v1.py

--- app_kayak_web_v1.py
+++ app_kayak_web_v1.py
@@ -200,6 +200,10 @@ def scraping_kayak(url, arrival, departure_date, arrival_date, temps_max):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
+    ## pour pouvoir utiliser --headless, il faut rajouter un user-agent
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+    chrome_options.add_argument(f'user-agent={user_agent}')
+    ##     ##
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
